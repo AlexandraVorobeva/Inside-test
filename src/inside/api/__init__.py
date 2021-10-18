45a81fc668efd2211cff6e0a8d3db898dc6a01ba
Aleksandra Vorobeva <V-Sash@mail.ru>
2021-10-18 13:19:06 +0300

    2

--- src/inside/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from .auth import router as auth_router
-from .messages import router as messages_router
-
-
-router = APIRouter()
-router.include_router(auth_router)
-router.include_router(messages_router)
--- src/inside/app.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from .api import router
-
-
-app = FastAPI(title="Inside", description="API by Aexandra Vorobeva")
-app.include_router(router)
-
-
